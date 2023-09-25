931b85cfa5850279aee22d5db298810c174bf2e1
rajakhan00 <145961512+rajakhan00@users.noreply.github.com>
2023-09-25 12:26:47 +0530

    Update .py

--- .py
+++ .py
@@ -1,4 +1,9 @@
 a,b,c,r,z=[int(x)for x in input('enter five numbers').split()]
 print('average'(a+b+c+r+z/5))
 
-output :- enter the number:79 89 78 76 89
+output :- enter the number:79 89 78 76 67
+---------------------------------------------------------------
+print("ansari adil")
+print("fycs")
+print
+---------------------------------------------------------------
